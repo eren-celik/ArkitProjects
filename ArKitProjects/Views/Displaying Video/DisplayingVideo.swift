77928cf5278e8590ac65daec47d84a024483196d
erennlk <elik.dark91@icloud.com>
2021-05-07 16:56:19 +0300

    Playing Video Added

---
+++ ArKitProjects/Views/Displaying Video/DisplayingVideo.swift
@@ -0,0 +1,89 @@
+//
+//  DisplayingView.swift
+//  ArKitProjects
+//
+//  Created by Eren  Çelik on 7.05.2021.
+//
+
+
+import UIKit
+import SceneKit
+import ARKit
+
+class VideoPlayerViewController: UIViewController, ARSCNViewDelegate {
+
+     var sceneView = ARSCNView(frame: screenBounds)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        registerGestureRecognizers()
+    }
+    
+    private func registerGestureRecognizers() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+        
+        guard let currentFrame = self.sceneView.session.currentFrame else {
+            return
+        }
+        
+        let videoNode = SKVideoNode(fileNamed: "Helper/big_buck_bunny.mp4")
+        videoNode.play()
+        
+        let skScene = SKScene(size: CGSize(width: 640, height: 480))
+        skScene.addChild(videoNode)
+        
+        videoNode.position = CGPoint(x: skScene.size.width/2, y: skScene.size.height/2)
+        videoNode.size = skScene.size
+        
+        let tvPlane = SCNPlane(width: 1.0, height: 0.75)
+        tvPlane.firstMaterial?.diffuse.contents = skScene
+        tvPlane.firstMaterial?.isDoubleSided = true
+        
+        let tvPlaneNode = SCNNode(geometry: tvPlane)
+        
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1.0
+        
+        tvPlaneNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        tvPlaneNode.eulerAngles = SCNVector3(Double.pi,0,0)
+        
+        self.sceneView.scene.rootNode.addChildNode(tvPlaneNode)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+ 
+}
